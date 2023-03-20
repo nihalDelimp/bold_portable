0f33424574544152c177079ee259f6fae42d9875
Nihal Chaurasia <nihal@delimp.com>
2023-03-20 15:36:42 +0530

    Renaming the name from User to Product

--- models/product/product.schema.js
+++ models/product/product.schema.js
@@ -31,6 +31,6 @@ const productSchema = new mongoose.Schema(
         }
     }
 );
-const User = mongoose.model('Product', productSchema);
+const Product = mongoose.model('Product', productSchema);
 
-module.exports = User;
+module.exports = Product;
