f421c0b2654444aa5f036080756edaabe1d5177d
Nihal Chaurasia <nihal@delimp.com>
2023-03-30 11:31:12 +0530

    New order recieved Socketio Changes

--- server.js
+++ server.js
@@ -15,5 +15,14 @@ const io = socket(server, {
 
 io.on("connection", (socket) => {
     console.log("Socket is connected")
+    // socket.emit('newOrder', order);
+    socket.on('new_order', (order) => {
+        // handle the new order event here, e.g. save the order to a database
+        console.log('new order received:', order);
+
+        // emit the new order event to the admin panel
+        io.emit('new_order_recieved', order);
+    });
 })
 
+module.exports = { server, io };
