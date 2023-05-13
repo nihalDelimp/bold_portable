c61c9b853eb9702b01dfb44b1ee5783e78e80191
Ivtidai <ivtidai.warsi@gmail.com>
2023-05-13 19:18:45 +0530

    Remove unused code

--- controller/stripe/subscriptions.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const subscriptionsController = require('./subscriptions.controller');
-// const checkAuth = require('../../middleware/checkAuth');
-// const { hasRole, hasMultipleRole } = require('../../middleware/checkRole');
-
-router.post('/create-subscriptions', subscriptionsController.createSubscription);
-
-module.exports = router;
