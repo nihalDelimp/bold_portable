8e19d6b1450faa5365fd5ee457df0d514266ade3
nihalDelimp <nihal@delimp.com>
2023-05-14 16:54:03 +0530

    Change in selecting up the schema

--- models/user/user.schema.js
+++ models/user/user.schema.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
             required: true,
-            select: false
+            // select: false   //Will look into this how to handle this
         },
         user_type: {
             type: String,
