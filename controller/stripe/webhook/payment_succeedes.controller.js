b4277b1ce1f5c8b0fb08f583fe9e3d5c1f47cf51
Amit <amit@delimp.com>
2023-06-16 19:22:10 +0530

    user-service save

--- controller/stripe/webhook/payment_succeedes.controller.js
+++ controller/stripe/webhook/payment_succeedes.controller.js
@@ -5,6 +5,7 @@ const logger = require("../../../helpers/logger.js");
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const qrcode = require('qrcode');
+const mailer = require("../../../helpers/nodemailer");
 
 exports.paymentSucceeded = async (object) => {
 
@@ -29,12 +30,14 @@ exports.paymentSucceeded = async (object) => {
         const { customer_email, subscription, status } = object;
 
         const user = await User.findOne({ email: customer_email });
+        
         let sub;
+
+        const serviceUrl = process.env.APP_URL+'/services?quotationType=' + product.metadata.quotationType + '&quotationId=' +product.metadata.quotationId;
+
+        const dataURL = await qrcode.toDataURL(serviceUrl);
+
         if (status === "paid") {
-
-            const serviceUrl = process.env.APP_URL+'/services?quotationType=' + product.metadata.quotationType + '&quotationId=' +product.metadata.quotationId;
-
-            const dataURL = await qrcode.toDataURL(serviceUrl);
 
             sub = await new Subscription({
                 user: user._id,
@@ -49,6 +52,22 @@ exports.paymentSucceeded = async (object) => {
                 subscription,
             });
         }
+
+        const mailOptions = {
+            from: 'hello@boldportable.com',
+            to: customer_email,
+            subject: 'QR Code for your Portable Rental',
+            text: `Hi,\n\nPlease find the atached QR code to scan and redirect to the service page  \n\nThanks,\nBold Portable Team`,
+            attachments: [
+                {
+                    filename: 'qrcode.png',
+                    content: dataURL.split(';base64,').pop(),
+                    encoding: 'base64'
+                }
+            ]
+        };
+        
+        mailer.sendMail(mailOptions);
 
         const payment = new Payment({
             subscription: sub._id,
