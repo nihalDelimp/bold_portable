08040cd54518d0e8774ff87c5c7463334b9535d8
tahseem <tahseem@delimp.com>
2023-07-13 16:42:13 +0530

    changed service notification query

--- controller/userService/userService.controller.js
+++ controller/userService/userService.controller.js
@@ -2,6 +2,10 @@ const UserService = require('../../models/userServices/userServices.schema');
 const apiResponse = require("../../helpers/apiResponse");
 const Notification = require('../../models/notification/notification.schema');
 const { server } = require('../../server');
+const DisasterRelief = require('../../models/disasterRelief/disasterRelief.schema');
+const PersonalOrBusiness = require('../../models/personalOrBusiness/personal_or_business_site.schema');
+const FarmOrchardWinery = require('../../models/farm_orchard_winery/farm_orchard_winery.schema');
+const Construction = require('../../models/construction/construction.schema');
 const io = require('socket.io')(server);
 
 exports.save = async (req, res) => {
@@ -24,9 +28,31 @@ exports.save = async (req, res) => {
             });
         }
 
+		let quotation;
+		switch (quotationType) {
+			case 'event':
+				quotation = await Event.findOne({ _id: quotationId }).populate({ path: "user", model: "User" });
+				break;
+			case 'farm-orchard-winery':
+				quotation = await FarmOrchardWinery.findOne({ _id: quotationId }).populate({ path: "user", model: "User" });
+				break;
+			case 'personal-or-business':
+				quotation = await PersonalOrBusiness.findOne({ _id: quotationId }).populate({ path: "user", model: "User" });
+				break;
+			case 'disaster-relief':
+				quotation = await DisasterRelief.findOne({ _id: quotationId }).populate({ path: "user", model: "User" });
+
+				break;
+			case 'construction':
+				quotation = await Construction.findOne({ _id: quotationId }).populate({ path: "user", model: "User" });
+				break;
+			default:
+				throw new Error(`Quotation type '${quotationType}' not found`).populate({ path: "user", model: "User" });
+		}
+
 		// Create a new UserServices instance with the extracted data
 		const newUserServices = new UserService({
-			// user: req.userData.user,
+			 user: quotation.user,
 			service,
 			serviceTypes,
 			quotationId,
@@ -43,16 +69,16 @@ exports.save = async (req, res) => {
 		// Save the new UserServices instance to the database
 		const savedUserServices = await newUserServices.save();
 
-		// Save the new Notification for Admmin panel 
-		// const notification = new Notification({
-		// 	user: req.userData.user,
-		// 	quote_type: quotationType,
-		// 	quote_id: quotationId,
-		// 	type: "SERVICE_REQUEST",
-		// 	status_seen: false
-		// });
+	//	Save the new Notification for Admmin panel 
+		const notification = new Notification({
+			user: quotation.user,
+			quote_type: quotationType,
+			quote_id: quotationId,
+			type: "SERVICE_REQUEST",
+			status_seen: false
+		});
 
-		// await notification.save();
+		await notification.save();
 
 		io.emit("user_service_saved", { savedUserServices });
 
