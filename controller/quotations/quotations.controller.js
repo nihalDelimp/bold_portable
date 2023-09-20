6e410b8ae360a5e13ec1d86df8dfba54e2873c1d
Amit <amit@delimp.com>
2023-09-20 16:43:26 +0530

    email to admin on quotation create

--- controller/quotations/quotations.controller.js
+++ controller/quotations/quotations.controller.js
@@ -166,9 +166,9 @@ exports.createConstructionQuotation = async (req, res) => {
         });
         await notification.save();
 
-        const emailModel = await AdminEmail.findOne({ slug: "construction-quotation-created" });
+        try {
+            const emailModel = await AdminEmail.findOne({ slug: "construction-quotation-created" });
 
-        if(emailModel) {
             const mailOptions = {
                 from: process.env.MAIL_FROM,
                 to: process.env.GO_BOLD_ADMIN_MAIL,
@@ -178,8 +178,10 @@ exports.createConstructionQuotation = async (req, res) => {
             };
             
             mailer.sendMail(mailOptions);
+        } catch (error) {
+            console.error('errrrrrrrrrrr', error);
+            throw new Error('err msggggggggg', innerError.message);
         }
-
         return apiResponse.successResponseWithData(
             res,
             "Quotation has been created successfully",
